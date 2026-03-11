8c4579c00567f3363525beb882565a74719e4d8d
nnennafavour <nnennafavour838@gmail.com>
2026-03-11 09:51:14 -0700

    initial commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,29 @@
+// // src/Pages/Home.jsx
+// import React from "react";
+// import Hero from "../Components/Hero";
+// // import Services from "../Components/Services";
+
+// const Home = () => {
+//   return (
+//     <>
+//       <Hero />
+//       {/* <Services /> */}
+//     </>
+//   );
+// };
+
+// export default Home;
+
+
+import React from "react";
+import Hero from "../Components/Hero";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default Home;
